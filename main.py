60a99393c3ab15fd786e543ab16991e484f08385
ryanhearts <ryan.jeong@petnow.io>
2022-06-28 16:14:32 +0900

    gender added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 
 l1 = [i for i in range(10)]
 l2 = list()
+l3 = list()
 alp = "abcdefghijk"
 
 for each in range(10):
@@ -11,6 +12,7 @@ for each in range(10):
         rand = random.randint(0, len(alp)-1)
         each_code += alp[rand]
     l2.append(each_code)
+    l3.append(random.randint(0,1))
 
-df = pd.DataFrame({'id': l1, 'code': l2})
+df = pd.DataFrame({'id': l1, 'code': l2, 'gender': l3})
 print(df)
